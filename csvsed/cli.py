087f0ddc0b89c0dd79c290b062beccd2073fbea2
Quentin Augé <qauge@dhatim.com>
2016-04-04 12:30:52 +0200

    Convert 2-spaces indents to 4-spaces indents
    Adopt CsvKit coding style

--- csvsed/cli.py
+++ csvsed/cli.py
@@ -11,52 +11,52 @@ from csvsed import sed
 
 class CsvSed(CSVKitUtility):
 
-  description = 'A stream-oriented CSV modification tool. Like a ' \
-                ' stripped-down "sed" command, but for tabular data.'
+    description = 'A stream-oriented CSV modification tool. Like a ' \
+                  ' stripped-down "sed" command, but for tabular data.'
 
-  def add_arguments(self):
-    self.argparser.add_argument('-n', '--names', dest='names_only', action='store_true',
-                                help='Display column names and indices from the input CSV and exit.')
-    self.argparser.add_argument('-c', '--columns', dest='columns',
-                                help='A comma separated list of column indices or names to be modified.')
-    self.argparser.add_argument('-m', '--modifier', dest='modifier',
-                                help='If specified, he "sed" modifier to evaluate: currently supports substitution '
-                                ' (s/REGEX/EXPR/FLAGS) and transliteration (y/SRC/DEST/FLAGS)')
+    def add_arguments(self):
+        self.argparser.add_argument('-n', '--names', dest='names_only', action='store_true',
+       	  help='Display column names and indices from the input CSV and exit.')
+        self.argparser.add_argument('-c', '--columns', dest='columns',
+          help='A comma separated list of column indices or names to be modified.')
+        self.argparser.add_argument('-m', '--modifier', dest='modifier',
+          help='If specified, he "sed" modifier to evaluate: currently supports substitution '
+          ' (s/REGEX/EXPR/FLAGS) and transliteration (y/SRC/DEST/FLAGS)')
 
-  def main(self):
-    if self.args.names_only:
-      self.print_column_names()
-      return
+    def main(self):
+        if self.args.names_only:
+            self.print_column_names()
+            return
 
-    if not self.args.columns:
-      self.argparser.error('You must specify at least one column to search using the -c option.')
+        if not self.args.columns:
+            self.argparser.error('You must specify at least one column to search using the -c option.')
 
-    if self.args.modifier is None:
-      self.argparser.error('-r must be specified, unless using the -n option.')
+        if self.args.modifier is None:
+            self.argparser.error('-r must be specified, unless using the -n option.')
 
-    # work exclusively with unicode modifiers
-    if isinstance(self.args.modifier, str):
-      self.args.modifier = self.args.modifier.decode('utf-8')
+        # work exclusively with unicode modifiers
+        if isinstance(self.args.modifier, str):
+            self.args.modifier = self.args.modifier.decode('utf-8')
 
-    reader_kwargs = self.reader_kwargs
-    writer_kwargs = self.writer_kwargs
-    if writer_kwargs.pop('line_numbers', False):
-      reader_kwargs = {'line_numbers': True}
+        reader_kwargs = self.reader_kwargs
+        writer_kwargs = self.writer_kwargs
+        if writer_kwargs.pop('line_numbers', False):
+            reader_kwargs = {'line_numbers': True}
 
-    rows, column_names, column_ids = self.get_rows_and_column_names_and_column_ids(**reader_kwargs)
+        rows, column_names, column_ids = self.get_rows_and_column_names_and_column_ids(**reader_kwargs)
 
-    modifiers = {idx: self.args.modifier for idx in column_ids}
-    reader = sed.CsvFilter(rows, modifiers, header=False)
+        modifiers = {idx: self.args.modifier for idx in column_ids}
+        reader = sed.CsvFilter(rows, modifiers, header=False)
 
-    output = agate.csv.writer(self.output_file, **writer_kwargs)
-    output.writerow(column_names)
+        output = agate.csv.writer(self.output_file, **writer_kwargs)
+        output.writerow(column_names)
 
-    for row in reader:
-      output.writerow(row)
+        for row in reader:
+            output.writerow(row)
 
 def launch_instance():
-  utility = CsvSed()
-  utility.main()
+    utility = CsvSed()
+    utility.main()
 
 if __name__ == '__main__':
-  launch_instance()
+    launch_instance()
